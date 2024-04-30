b73a81e56290d06267a3ce7184d6c3596e5c787d
Aleix Batlle <aleixbatlle@gmail.com>
2024-05-01 00:33:17 +0200

    Update background styling and text content in App.css and App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import './App.css'
 
 function App() {
   return (
+    <div className="bgImage">
       <div className="bg">
           Vienen cosas interesantes en esta WEB en construcción
       </div>
+    </div>
   )
 }
 
